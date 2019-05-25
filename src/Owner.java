5029953ce36e8d7ac267a780a9ff55d1a58a8beb
SLiFER999 <jogospg1999@gmail.com>
2019-05-25 14:18:45 +0100

    merges

--- src/Owner.java
+++ src/Owner.java
@@ -1,8 +1,5 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Owner extends GeneralUser
@@ -126,3 +123,4 @@ public class Owner extends GeneralUser
     //
 
 }
+
